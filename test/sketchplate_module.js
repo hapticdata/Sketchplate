e91c05dee5caf0ef161e425773c0933b1102785f
Kyle Phillips <kyle@haptic-data.com>
2012-08-19 19:54:07 -0500

    refactored to create a Project module that manages any functionality for a template-project after it has been copied, altered unit tests and bin

--- test/sketchplate_module.js
+++ test/sketchplate_module.js
@@ -4,32 +4,24 @@ var sketchplate = require('../lib/sketchplate');
 describe('Creates a new default project', function (){
 
 	var projectLocation = '../test-project';
-	var project = sketchplate.create( projectLocation, require('../settings.json') );
+	var plate = sketchplate.create( require('../settings.json') );
 
 	it('should create a new project', function ( done ){
 		this.timeout( 120000 );
-		project.copyTemplate(function (err, location ){
-			if( err ){
-				console.log('error');
-				throw err;
-			}
-			done();
-		});
-	});
-
-	it('should create a git repository', function (done ){
-		project.initRepo(function ( code ){
-			if( code !== 0 )
-				throw new Error("Git repo initialization failed with code: "+ code );
-			done();
-		});
-	});
-
-	it('should open sublime text', function ( done ){
-		project.openInEditor(function (err ){
-			if( err )
-				throw err;
-			done();
+		//copy template
+		plate.copyTemplate( projectLocation, function (err, project ){
+			if( err ) throw err;
+			//take created project and init repo
+			project.initRepo(function ( code ){
+				if( code !== 0 )
+					throw new Error("Git repo initialization failed with code: "+ code );
+			});
+			//take created project and open in editor
+			project.openInEditor(function (err ){
+				if( err )
+					throw err;
+				done();
+			});
 		});
 	});
 });
