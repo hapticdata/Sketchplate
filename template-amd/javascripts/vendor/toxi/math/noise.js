9fbf7dca434638cb6fbb6aa4d21a73fb59df8837
Kyle Phillips <kyle@haptic-data.com>
2012-08-23 02:33:25 -0500

    now supports loading multiple templates and generic resources

--- template-amd/javascripts/vendor/toxi/math/noise.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports", "module", "./noise/PerlinNoise","./noise/simplexNoise"], function(require, exports, module) {
-/** module toxi/math/noise 
-	@api public
-*/
-module.exports = {
-	PerlinNoise: require('./noise/PerlinNoise'),
-	simplexNoise: require('./noise/simplexNoise')	
-};
-});
