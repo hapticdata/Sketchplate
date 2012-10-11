74bf2d1f4d89c0b0d7af3961f90a9f288eac82ff
Kyle Phillips <kyle@haptic-data.com>
2012-10-11 10:58:19 -0500

    fetch now supports zipballs

--- test/fetch.js
+++ test/fetch.js
@@ -1,12 +1,13 @@
+/*global describe, it*/
 var fetch = require('../lib/fetch');
 
 
 describe('fetch', function(){
-/*
+	var tmp = 'test_downloads/';
 	it('should download async.js from a url', function( done ){
 		fetch({
 				"url": "https://raw.github.com/caolan/async/master/lib/async.js",
-				"target": "javascripts/vendor/async.js"
+				"target": tmp + "vendor/async.js"
 			})
 			.on('exit',function( err ){
 				if( err ) throw err;
@@ -19,7 +20,7 @@ describe('fetch', function(){
 		fetch({
 				"git": "https://code.google.com/p/dat-gui/",
 				"src": "src/dat",
-				"target": "javascripts/vendor/dat"
+				"target": tmp + "vendor/dat"
 			})
 			.on('exit', function( err ){
 				if( err ) throw err;
@@ -27,24 +28,11 @@ describe('fetch', function(){
 			});
 	});
 
-	it('should clone toxiclibsjs with git and nest it', function( done ){
+	it('should download bootstrap as a zipball', function( done ){
 		this.timeout( 120000 );
 		fetch({
-				"git": "https://github.com/hapticdata/toxiclibsjs.git",
-				"src": "lib/toxi",
-				"target": "javascripts/really/deep/directory/toxi"
-			})
-			.on('exit', function( err ){
-				if( err ) throw err;
-				done();
-			});
-	});*/
-
-	it('should download tarball', function( done ){
-		this.timeout( 120000 );
-		fetch({
-			"zip": "https://github.com/hapticdata/adm-zip/zipball/master",
-			"target": "javascripts/tar-async"
+			"zip": "https://github.com/twitter/bootstrap/zipball/master",
+			"target": tmp + "bootstrap/"
 		})
 		.on('exit', function( err ){
 			if( err ) throw err;
