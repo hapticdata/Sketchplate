6d122eb2419a03e48b4e0ab94a17c1a58c39a94f
Kyle Phillips <kyle@haptic-data.com>
2012-11-08 01:13:39 +0000

    made confirmation for sketch plate when editor isn't set correctly. made Project.js hooks.js and created exports not tied to a project.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 	//SETUP EDITOR
 	//**Command Array to launch editor**
 	//the path to the project will be appended as the last argument
-	
+
 	//Default: Sublime Text 2 for Mac
 	editorArgs: [
 		"/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
