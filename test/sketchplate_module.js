ee4095b529494a909be2622d675d675bbb632143
Kyle Phillips <kyle.phillips@b-reel.com>
2013-11-21 00:10:31 -0800

    bugfix plate#fetchAll calling callback for each library, resulting in incomplete task. added unit test

--- test/sketchplate_module.js
+++ test/sketchplate_module.js
@@ -1,27 +1,39 @@
 /*global describe, it*/
-var sketchplate = require('../lib/sketchplate');
+var sketchplate = require('../lib/sketchplate'),
+    _ = require('underscore'),
+    config = require('../lib/config');
 
 describe('Creates a new default project', function (){
 
 	var projectLocation = '../test-project';
-	var plate = sketchplate.create( require('../settings.json') );
+	var plate = sketchplate.create(_.defaults({ template: 'amd-sketch' }, config.getUserConfig()));
 
-	it('should create a new project', function ( done ){
-		this.timeout( 120000 );
-		//copy template
-		plate.copyTemplate( projectLocation, function (err, project ){
-			if( err ) throw err;
-			//take created project and init repo
-			project.initRepo(function ( code ){
-				if( code !== 0 )
-					throw new Error("Git repo initialization failed with code: "+ code );
-			});
-			//take created project and open in editor
-			project.openInEditor(function (err ){
-				if( err )
-					throw err;
-				done();
-			});
-		});
-	});
+    describe.only('plate#fetchAll', function(){
+        it('should download all fetch-resources', function( done ){
+            this.timeout( 120000 );
+            var i = 0;
+            plate.fetchAll(done);
+        });
+    });
+
+    describe('plate#copyTemplate', function(){
+        it('should create a new project', function ( done ){
+            this.timeout( 120000 );
+            //copy template
+            plate.copyTemplate( projectLocation, function (err, project ){
+                if( err ) throw err;
+                //take created project and init repo
+                project.initRepo(function ( code ){
+                    if( code !== 0 )
+                        throw new Error("Git repo initialization failed with code: "+ code );
+                });
+                //take created project and open in editor
+                project.openInEditor(function (err ){
+                    if( err )
+                        throw err;
+                    done();
+                });
+            });
+        });
+    });
 });
