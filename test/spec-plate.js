90fc8ecd0daf0c2e698e597ddb8e88d711f96dee
Kyle Phillips <kyle.phillips@b-reel.com>
2014-06-27 17:13:29 -0700

    fix test/spec-plate.js and fix fetchAll not completing
    refactor Project to hooks.createProject

--- test/spec-plate.js
+++ test/spec-plate.js
@@ -9,36 +9,37 @@ describe('Creates a new default project', function (){
 	var projectLocation = './test_downloads/test-project';
 	var plate = sketchplate.create(_.defaults({ template: 'amd-sketch' }, config.getUserConfig()));
 
-    describe.only('sketchplate.create', function(){
+    describe('sketchplate.create', function(){
         it('should be a plate for `amd-sketch` template', function(){
             assert.equal( typeof plate.copyTemplate, 'function' );
-            assert.equal( typeof plate.templatesPath, 'string' );
+            assert.equal( typeof plate.templatePath, 'string' );
         });
     });
 
     describe('plate#fetchAll', function(){
         it('should download all fetch-resources', function( done ){
-            this.timeout( 120000 );
-            var i = 0;
-            plate.fetchAll(done);
+            this.timeout( 180000 );
+            var onComplete = function(){
+                done();
+            };
+            var onProgress = function(err, msg){
+                assert.equal( err, null );
+                assert.equal( typeof msg.id, 'string' );
+            };
+            plate.fetchAll( onComplete, onProgress );
         });
     });
 
     describe('plate#copyTemplate', function(){
         it('should create a new project', function ( done ){
-            this.timeout( 120000 );
+            this.timeout( 180000 );
             //copy template
             plate.copyTemplate( projectLocation, function (err, project ){
                 if( err ) throw err;
                 //take created project and init repo
-                project.initRepo(function ( code ){
-                    if( code !== 0 )
-                        throw new Error("Git repo initialization failed with code: "+ code );
-                });
-                //take created project and open in editor
-                project.openInEditor(function (err ){
-                    if( err )
-                        throw err;
+                project.gitInit(function ( err, actionsPerformed ){
+                    assert.equal( err, null );
+                    assert.ok( actionsPerformed.length > 0 );
                     done();
                 });
             });
